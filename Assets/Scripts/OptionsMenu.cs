452017064e357e1ff92cd0754f8c8a99368c383e
PopeGregory <45657904+PopeGregory@users.noreply.github.com>
2023-01-16 11:37:14 +0700

    Main menu and options(volume and fullscreen)

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer soundMixer;
+    public AudioMixer musicMixer;
+    public void SetSoundVolume(float soundvolume)
+    {
+        soundMixer.SetFloat("SoundVolume",soundvolume);
+    }
+    public void SetMusicVolume(float musicvolume)
+    {
+        musicMixer.SetFloat("MusicVolume", musicvolume);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
